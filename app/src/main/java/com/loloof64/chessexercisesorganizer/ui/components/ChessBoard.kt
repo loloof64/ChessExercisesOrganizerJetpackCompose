2dc73ca5d0ac328d9eec54acf15de3c2308720e0
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-05-01 17:30:57 +0200

    Implemented basic Drag and Drop : without being validated.

--- app/src/main/java/com/loloof64/chessexercisesorganizer/ui/components/ChessBoard.kt
+++ app/src/main/java/com/loloof64/chessexercisesorganizer/ui/components/ChessBoard.kt
@@ -3,10 +3,14 @@ package com.loloof64.chessexercisesorganizer.ui.components
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -14,16 +18,30 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.alonsoruibal.chess.Board
 import com.loloof64.chessexercisesorganizer.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.floor
 
 const val STANDARD_FEN = Board.FEN_START_POSITION
 
 const val NO_PIECE = '.'
+
+fun Char.isWhitePiece(): Boolean {
+    return when (this) {
+        'P', 'N', 'B', 'R', 'Q', 'K' -> true
+        'p', 'n', 'b', 'r', 'q', 'k' -> false
+        else -> throw IllegalArgumentException("Not a valid piece : $this !")
+    }
+}
 
 fun Char.getPieceImageID(): Int {
     return when (this) {
@@ -50,22 +68,207 @@ fun String.toBoard(): Board {
     }
 }
 
+data class DndData(
+    val pieceValue: Char = NO_PIECE,
+    val startFile: Int = Int.MIN_VALUE,
+    val startRank: Int = Int.MIN_VALUE,
+    val targetFile: Int = Int.MIN_VALUE,
+    val targetRank: Int = Int.MIN_VALUE,
+    val movedPieceXRatio: Float = Float.MIN_VALUE,
+    val movedPieceYRatio: Float = Float.MAX_VALUE,
+) {
+    override fun toString(): String = "$pieceValue|$startFile|$startRank|$targetFile|$targetRank|" +
+            "$movedPieceXRatio|$movedPieceYRatio"
+
+    companion object {
+        fun parse(valueStr: String): DndData {
+            try {
+                val parts = valueStr.split("|")
+                return DndData(
+                    pieceValue = parts[0][0],
+                    startFile = parts[1].toInt(),
+                    startRank = parts[2].toInt(),
+                    targetFile = parts[3].toInt(),
+                    targetRank = parts[4].toInt(),
+                    movedPieceXRatio = parts[5].toFloat(),
+                    movedPieceYRatio = parts[6].toFloat(),
+                )
+            } catch (ex: NumberFormatException) {
+                return DndData()
+            } catch (ex: ArrayIndexOutOfBoundsException) {
+                return DndData()
+            }
+        }
+    }
+}
+
+
+val DndDataStateSaver = Saver<DndData, String>(
+    save = { state -> state.toString() },
+    restore = { value ->
+        DndData.parse(value)
+    }
+)
+
+
 @Composable
 fun DynamicChessBoard(
     modifier: Modifier = Modifier,
     position: String = STANDARD_FEN,
     reversed: Boolean = false,
 ) {
+    val composableScope = rememberCoroutineScope()
+
     val boardLogic = position.toBoard()
     val currentContext = LocalContext.current
+
+    var dndState by rememberSaveable(stateSaver = DndDataStateSaver) {
+        mutableStateOf(DndData())
+    }
+
+    var cellsSize by remember { mutableStateOf(0f) }
+
+    fun processDragAndDropStart(file: Int, rank: Int, piece: Char) {
+        val whiteTurn = boardLogic.turn
+        val isPieceOfSideToMove =
+            (piece.isWhitePiece() && whiteTurn) ||
+                    (!piece.isWhitePiece() && !whiteTurn)
+        if (isPieceOfSideToMove) {
+            val col = if (reversed) 7 - file else file
+            val row = if (reversed) rank else 7 - rank
+            val boardMinSize = cellsSize * 9f
+            val newMovedPieceX = cellsSize * (0.5f + col.toFloat())
+            val newMovedPieceY = cellsSize * (0.5f + row.toFloat())
+            val newMovedPieceXRatio = newMovedPieceX / boardMinSize
+            val newMovedPieceYRatio = newMovedPieceY / boardMinSize
+            dndState = dndState.copy(
+                startFile = file,
+                startRank = rank,
+                movedPieceXRatio = newMovedPieceXRatio,
+                movedPieceYRatio = newMovedPieceYRatio,
+                pieceValue = piece
+            )
+        }
+    }
+
+    fun cancelDragAndDropAnimation() {
+        val initialXRatio = dndState.movedPieceXRatio
+        val initialYRatio = dndState.movedPieceYRatio
+
+        val startCol = if (reversed) 7 - dndState.startFile else dndState.startFile
+        val startRow = if (reversed) dndState.startRank else 7 - dndState.startRank
+
+        val targetX = cellsSize * (0.5f + startCol)
+        val targetY = cellsSize * (0.5f + startRow)
+        val minBoardSize = cellsSize * 9f
+        val targetXRatio = targetX / minBoardSize
+        val targetYRatio = targetY / minBoardSize
+
+        val animationDurationMillis = 250
+
+        dndState = dndState.copy(
+            targetFile = Int.MIN_VALUE,
+            targetRank = Int.MIN_VALUE
+        )
+
+        val currentXRatio = Animatable(initialXRatio)
+        val currentYRatio = Animatable(initialYRatio)
+
+        composableScope.launch {
+            currentXRatio.animateTo(
+                targetXRatio,
+                animationSpec = TweenSpec(
+                    durationMillis = animationDurationMillis,
+                    easing = LinearEasing
+                )
+            ) {
+                dndState = dndState.copy(
+                    movedPieceXRatio = value
+                )
+            }
+        }
+
+        composableScope.launch {
+            currentYRatio.animateTo(
+                targetYRatio,
+                animationSpec = TweenSpec(
+                    durationMillis = animationDurationMillis,
+                    easing = LinearEasing
+                )
+            ) {
+                dndState = dndState.copy(
+                    movedPieceYRatio = value
+                )
+            }
+        }
+
+        composableScope.launch {
+            delay(animationDurationMillis.toLong())
+            dndState = DndData()
+        }
+    }
+
+
+    fun handleDragStart(offset: Offset) {
+        val col = floor((offset.x - cellsSize * 0.5f) / cellsSize).toInt()
+        val row = floor((offset.y - cellsSize * 0.5f) / cellsSize).toInt()
+
+        val outOfBounds = col < 0 || col > 7 || row < 0 || row > 7
+        if (!outOfBounds) {
+            val file = if (reversed) 7 - col else col
+            val rank = if (reversed) row else 7 - row
+            val square = getSquareFromCellCoordinates(file, rank)
+            val piece = boardLogic.getPieceAt(square)
+
+            if (piece != NO_PIECE) {
+                processDragAndDropStart(file, rank, piece)
+            }
+        }
+    }
+
+    fun handleDragMove(change: PointerInputChange, dragAmount: Offset) {
+        change.consumeAllChanges()
+
+        if (dndState.pieceValue != NO_PIECE) {
+            val boardMinSize = cellsSize * 9f
+            val newMovedPieceX = dndState.movedPieceXRatio * boardMinSize + dragAmount.x
+            val newMovedPieceY = dndState.movedPieceYRatio * boardMinSize + dragAmount.y
+            val newMovedPieceXRatio = newMovedPieceX / boardMinSize
+            val newMovedPieceYRatio = newMovedPieceY / boardMinSize
+            val newCol = floor((newMovedPieceX - cellsSize * 0.5f) / cellsSize).toInt()
+            val newRow = floor((newMovedPieceY - cellsSize * 0.5f) / cellsSize).toInt()
+            val targetFile = if (reversed) 7 - newCol else newCol
+            val targetRank = if (reversed) newRow else 7 - newRow
+            dndState = dndState.copy(
+                movedPieceXRatio = newMovedPieceXRatio,
+                movedPieceYRatio = newMovedPieceYRatio,
+                targetFile = targetFile,
+                targetRank = targetRank
+            )
+        }
+    }
+
+    fun handleDndCancel() {
+        cancelDragAndDropAnimation()
+    }
 
     Canvas(
         modifier = modifier
             .background(Color(214, 59, 96))
+            .pointerInput(reversed) {
+                detectDragGestures(
+                    onDragStart = { handleDragStart(it) },
+                    onDrag = { change, dragAmount ->
+                        handleDragMove(change, dragAmount)
+                    },
+                    onDragCancel = { handleDndCancel() },
+                    onDragEnd = { handleDndCancel() },
+                )
+            }
     ) {
         val minSize = if (size.width < size.height) size.width else size.height
-        val cellsSize = minSize / 9f
-        drawCells(cellsSize)
+        cellsSize = minSize / 9f
+        drawCells(cellsSize = cellsSize, reversed = reversed, dndData = dndState)
         drawFilesCoordinates(cellsSize, reversed)
         drawRanksCoordinates(cellsSize, reversed)
         drawPlayerTurn(cellsSize, boardLogic)
@@ -74,7 +277,23 @@ fun DynamicChessBoard(
             cellsSize = cellsSize,
             position = boardLogic,
             reversed = reversed,
+            dndData = dndState,
         )
+
+        if (dndState.pieceValue != NO_PIECE) {
+            val boardMinSize = cellsSize * 9f
+            val x = boardMinSize * dndState.movedPieceXRatio
+            val y = boardMinSize * dndState.movedPieceYRatio
+
+            drawMovedPiece(
+                context = currentContext,
+                cellsSize = cellsSize.toInt(),
+                pieceValue = dndState.pieceValue,
+                x = x,
+                y = y,
+                positionFen = boardLogic.fen,
+            )
+        }
 
     }
 }
@@ -112,12 +331,24 @@ private fun getSquareFromCellCoordinates(file: Int, rank: Int): Long {
     return 1L.shl(7 - file + 8 * rank)
 }
 
-private fun DrawScope.drawCells(cellsSize: Float) {
+private fun DrawScope.drawCells(
+    cellsSize: Float,
+    reversed: Boolean = false,
+    dndData: DndData = DndData()
+) {
     repeat(8) { row ->
+        val rank = if (reversed) row else 7 - row
         repeat(8) { col ->
+            val file = if (reversed) 7 - col else col
+            val isDndStartCell = (file == dndData.startFile) && (rank == dndData.startRank)
+            val isDndTargetCell = (file == dndData.targetFile) && (rank == dndData.targetRank)
+            val isDndCrossCell = (file == dndData.targetFile) || (rank == dndData.targetRank)
             val isWhiteCell = (row + col) % 2 == 0
             val backgroundColor =
                 when {
+                    isDndTargetCell -> Color(112, 209, 35)
+                    isDndStartCell -> Color(214, 59, 96)
+                    isDndCrossCell -> Color(178, 46, 230)
                     isWhiteCell -> Color(255, 206, 158)
                     else -> Color(209, 139, 71)
                 }
@@ -192,6 +423,7 @@ private fun DrawScope.drawPieces(
     cellsSize: Float,
     position: Board,
     reversed: Boolean,
+    dndData: DndData = DndData(),
 ) {
     repeat(8) { row ->
         val rank = if (reversed) row else 7 - row
@@ -201,22 +433,50 @@ private fun DrawScope.drawPieces(
             val square = getSquareFromCellCoordinates(file, rank)
             val piece = position.getPieceAt(square)
             if (piece != NO_PIECE) {
-                val x = cellsSize * (0.5f + col)
-                val y = cellsSize * (0.5f + row)
-                val imageRef = piece.getPieceImageID()
-                val vectorDrawable =
-                    VectorDrawableCompat.create(context.resources, imageRef, null)
-                drawIntoCanvas {
-                    if (vectorDrawable != null) it.nativeCanvas.drawVector(
-                        vectorDrawable,
-                        x,
-                        y,
-                        cellsSize.toInt(),
-                        cellsSize.toInt()
-                    )
+                val isDraggedPiece = (dndData.startFile == file) && (dndData.startRank == rank)
+                if (!isDraggedPiece) {
+                    val x = cellsSize * (0.5f + col)
+                    val y = cellsSize * (0.5f + row)
+                    val imageRef = piece.getPieceImageID()
+                    val vectorDrawable =
+                        VectorDrawableCompat.create(context.resources, imageRef, null)
+                    drawIntoCanvas {
+                        if (vectorDrawable != null) it.nativeCanvas.drawVector(
+                            vectorDrawable,
+                            x,
+                            y,
+                            cellsSize.toInt(),
+                            cellsSize.toInt()
+                        )
+                    }
                 }
             }
         }
+    }
+}
+
+private fun DrawScope.drawMovedPiece(
+    context: Context,
+    cellsSize: Int,
+    pieceValue: Char,
+    x: Float,
+    y: Float,
+    positionFen: String
+) {
+    val boardLogic = Board()
+    boardLogic.fen = positionFen
+    val imageRef = pieceValue.getPieceImageID()
+
+    val vectorDrawable =
+        VectorDrawableCompat.create(context.resources, imageRef, null)
+    drawIntoCanvas {
+        if (vectorDrawable != null) it.nativeCanvas.drawVector(
+            vectorDrawable,
+            x,
+            y,
+            cellsSize,
+            cellsSize
+        )
     }
 }
 
