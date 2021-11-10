1e228694481975d6e9b784265d66bf72f2aeb6e8
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-11-10 16:00:19 +0100

    Added buttons for MovesNavigator component : but are not reactive yet.

--- app/src/main/java/com/loloof64/chessexercisesorganizer/ui/components/MovesNavigator.kt
+++ app/src/main/java/com/loloof64/chessexercisesorganizer/ui/components/MovesNavigator.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowLeft
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material.icons.filled.FirstPage
+import androidx.compose.material.icons.filled.LastPage
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,6 +19,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.flowlayout.FlowRow
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import com.loloof64.chessexercisesorganizer.R
 
 sealed class MovesNavigatorElement(
     open val text: String,
@@ -34,6 +40,33 @@ data class HalfMoveSAN(
 ) : MovesNavigatorElement(text, fen, lastMoveArrowData)
 
 @Composable
+fun MovesNavigatorButtons(
+    modifier: Modifier = Modifier,
+    handleFirstPositionRequest: () -> Unit = {},
+    handleLastPositionRequest: () -> Unit = {},
+    handlePreviousPositionRequest: () -> Unit = {},
+    handleNextPositionRequest: () -> Unit = {},
+) {
+    val iconsModifier = Modifier.size(128.dp)
+    val iconsTint = Color.Blue
+    Row(modifier = modifier
+        .fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        IconButton(onClick = handleFirstPositionRequest) {
+            Icon(contentDescription = stringResource(id = R.string.first_position), imageVector = Icons.Filled.FirstPage, tint = iconsTint, modifier = iconsModifier)
+        }
+        IconButton(onClick = handlePreviousPositionRequest) {
+            Icon(contentDescription = stringResource(id = R.string.previous_position), imageVector = Icons.Filled.ArrowLeft, tint = iconsTint, modifier = iconsModifier)
+        }
+        IconButton(onClick = handleNextPositionRequest) {
+            Icon(contentDescription = stringResource(id = R.string.next_position), imageVector = Icons.Filled.ArrowRight, tint = iconsTint, modifier = iconsModifier)
+        }
+        IconButton(onClick = handleLastPositionRequest) {
+            Icon(contentDescription = stringResource(id = R.string.last_position), imageVector = Icons.Filled.LastPage, tint = iconsTint, modifier = iconsModifier)
+        }
+    }
+}
+
+@Composable
 fun MovesNavigator(
     modifier: Modifier = Modifier,
     elements: Array<MovesNavigatorElement>,
@@ -45,25 +78,37 @@ fun MovesNavigator(
     val scrollAmount = ((mustBeVisibleByDefaultElementIndex / 6) * lineHeightPixels).toInt()
     val vertScrollState = ScrollState(scrollAmount)
 
-    FlowRow(
+    Column(
         modifier = modifier
             .background(color = Color.Yellow.copy(alpha = 0.3f))
-            .verticalScroll(vertScrollState),
-        mainAxisSpacing = 8.dp,
     ) {
-        elements.mapIndexed { index, elt ->
-            val backgroundColor =
-                if (index == highlightedItemIndex) Color.Green else Color.Transparent
-            Text(text = elt.text,
-                fontSize = 34.sp,
-                color = Color.Blue,
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier
-                    .background(color = backgroundColor)
-                    .clickable {
-                        if (elt.fen != null) elementSelectionRequestCallback(Triple(elt.fen!!, elt.lastMoveArrowData!!, index))
-                    }
-            )
+        MovesNavigatorButtons()
+
+        FlowRow(
+            modifier = Modifier
+                .verticalScroll(vertScrollState),
+            mainAxisSpacing = 8.dp,
+        ) {
+            elements.mapIndexed { index, elt ->
+                val backgroundColor =
+                    if (index == highlightedItemIndex) Color.Green else Color.Transparent
+                Text(text = elt.text,
+                    fontSize = 34.sp,
+                    color = Color.Blue,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .background(color = backgroundColor)
+                        .clickable {
+                            if (elt.fen != null) elementSelectionRequestCallback(
+                                Triple(
+                                    elt.fen!!,
+                                    elt.lastMoveArrowData!!,
+                                    index
+                                )
+                            )
+                        }
+                )
+            }
         }
     }
 }
